d2e57dc94a37212d3c14d8dcd9ce1df126bbdd88
sajaljn26 <sajaljn11@gmail.com>
2026-01-08 16:35:21 +0530

    added

---
+++ apps/media/src/main.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import { MediaModule } from './media.module';
+import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+
+  process.title = "media";
+  const logger = new Logger("MediaBootstrap");
+  const port = Number(process.env.MEDIA_TCP_PORT) || 4012;
+
+
+
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    MediaModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '0.0.0.0',
+        port: port
+      }
+    }
+  )
+
+  app.enableShutdownHooks()
+  await app.listen();
+
+  logger.log(`Media is running on port ${port}`)
+
+
+}
+bootstrap();
---
+++ apps/search/src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { SearchModule } from './search.module';
+import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+
+  process.title = "search"
+  const logger = new Logger("SearchBootstrap")
+  const port = Number(process.env.SEARCH_TCP_PORT) || 4013
+
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    SearchModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '0.0.0.0',
+        port: port
+      }
+    }
+  )
+  app.enableShutdownHooks();
+
+
+  await app.listen();
+  logger.log(`Search is running on port ${port}`)
+}
+bootstrap();
